ffcba9c1ccc76dc012ecd7277f954e258afc0e92
jonathan hernandez <jonathanhernandez@jonathans-imac>
2017-12-26 12:19:02 -0600

    persisting data using CoreData

--- CoreTodo/Controller/CategoryViewController.swift
+++ CoreTodo/Controller/CategoryViewController.swift
@@ -30,6 +30,20 @@ class CategoryViewController: UITableViewController {
         return cell
     }
     
+    //MARK: - Table Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories[indexPath.row]
+        }
+    }
+    
     //MARK: - Data MAnipulation Methods
     func saveCategories()  {
     
